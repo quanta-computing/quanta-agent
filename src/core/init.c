b45384ca9ebba462afb247e71a40fd4c6edd7c19
Matthieu Rosinski <korrigan@slash16.org>
2016-01-13 19:13:07 +0100

    run() is now based on select timeout

--- src/core/init.c
+++ src/core/init.c
@@ -8,6 +8,7 @@ int monikor_init(monikor_t *mon, const char *config_path) {
     return 1;
   monikor_setup_config(mon->config);
   monikor_logger_init(mon->config->log_level);
+  monikor_io_handler_list_init(&mon->io_handlers);
   mod_size = mon->config->modules.modules->size * sizeof(*mon->modules.modules);
   mon->modules.count = 0;
   if (!(mon->modules.modules = malloc(mod_size))
@@ -15,5 +16,9 @@ int monikor_init(monikor_t *mon, const char *config_path) {
     monikor_log(LOG_CRIT, "Cannot allocate memory\n");
     return 1;
   }
-  return monikor_load_all_modules(mon);
+  if (monikor_load_all_modules(mon))
+    return 1;
+  if (mon->config->unix_sock_path)
+    monikor_server_init(&mon->server, mon);
+  return 0;
 }
--- src/core/run.c
+++ src/core/run.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
 
@@ -25,25 +27,23 @@ static int poll_modules(monikor_t *mon) {
   return err;
 }
 
-static int perform_sleep(monikor_t *mon) {
-  int sleep_amount;
-  struct timeval now;
-
-  gettimeofday(&now, NULL);
-  sleep_amount = 1000000 * (mon->last_clock.tv_sec + mon->config->poll_interval - now.tv_sec);
-  sleep_amount += mon->last_clock.tv_usec - now.tv_usec;
-  if (sleep_amount < 0)
-    return 0;
-  monikor_log(LOG_DEBUG, "Sleeping for %dms\n", sleep_amount / 1000);
-  usleep(sleep_amount);
-  return 0;
-}
-
 int monikor_run(monikor_t *mon) {
+  struct timeval interval;
+  struct timeval now;
+  struct timeval next_update;
+
   while (42) {
     gettimeofday(&mon->last_clock, NULL);
+    next_update = mon->last_clock;
+    next_update.tv_sec += mon->config->poll_interval;
     poll_modules(mon);
-    perform_sleep(mon);
+    for (gettimeofday(&now, NULL); now.tv_sec < next_update.tv_sec; gettimeofday(&now, NULL)) {
+      interval.tv_sec = next_update.tv_sec - now.tv_sec;
+      interval.tv_usec = 0;
+      monikor_log(LOG_DEBUG, "Sleeping for %d\n", interval.tv_sec);
+      if (monikor_io_handler_poll(&mon->io_handlers, &interval) == -1)
+        monikor_log(LOG_ERR, "Error in select(2): %s\n", strerror(errno));
+    }
   }
   return 0;
 }
