e392bf41b938027148ef700cfbf97a912e3eee50
Matthieu Rosinski <korrigan@slash16.org>
2016-02-19 10:34:08 +0100

    added apache.workers.total metric

--- lib/modules/apache/poll.c
+++ lib/modules/apache/poll.c
@@ -15,19 +15,20 @@ static const struct {
   {NULL, NULL, 0}
 };
 
-static int fetch_metric(const char *apache_status, const char *name, unsigned *value) {
+static int fetch_metric(const char *apache_status, const char *name, uint64_t *value) {
   char *line;
 
   if (!(line = strstr(apache_status, name)))
     return 1;
-  *value = strtol(line + strlen(name), NULL, 10);
+  *value = strtoull(line + strlen(name), NULL, 10);
   return 0;
 }
 
 int apache_poll_metrics(monikor_t *mon, struct timeval *clock, const char *url) {
   int n = 0;
   http_response_t *status;
-  unsigned value;
+  uint64_t value;
+  uint64_t total_workers = 0;
 
   if (!(status = monikor_http_get(url, APACHE_TIMEOUT)))
     return -1;
@@ -40,13 +41,17 @@ int apache_poll_metrics(monikor_t *mon, struct timeval *clock, const char *url) 
     if (!fetch_metric(status->data, apache_metrics[i].field_name, &value)) {
       if (!strcmp(apache_metrics[i].name, "apache.workers.bytes_per_second"))
         value *= 1024;
+      if (!strcmp(apache_metrics[i].name, "apache.workers.busy")
+      || !strcmp(apache_metrics[i].name, "apache.workers.idle"))
+        total_workers += value;
       monikor_metric_push(mon, monikor_metric_integer(
         apache_metrics[i].name, clock, value, apache_metrics[i].flags
       ));
       n++;
     }
   }
+  monikor_metric_push(mon, monikor_metric_integer("apache.workers.total", clock, total_workers, 0));
   free(status->data);
   free(status);
-  return n;
+  return n + 1;
 }
