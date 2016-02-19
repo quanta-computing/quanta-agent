f38d4629a83b7ff713c4af2c5b5ad80c318ad542
Matthieu Rosinski <korrigan@slash16.org>
2016-02-19 10:33:45 +0100

    added hit/miss metrics to memcached

--- lib/modules/memcached/poll.c
+++ lib/modules/memcached/poll.c
@@ -7,6 +7,43 @@
 #include "memcached.h"
 
 static const struct {
+  char *hit;
+  char *miss;
+} hitmiss_metrics[] = {
+  {"get_hits", "get_misses"},
+  {"delete_hits", "delete_misses"},
+  {"incr_hits", "incr_misses"},
+  {"decr_hits", "decr_misses"},
+  {"cas_hits", "cas_misses"},
+  {"touch_hits", "touch_misses"},
+  {NULL, NULL}
+};
+
+static void fetch_hitmiss_metrics(monikor_t *mon, struct timeval *clock, char *status) {
+  char metric_name[256];
+  char *metric;
+  uint64_t hits = 0;
+  uint64_t misses = 0;
+
+  for (size_t i = 0; hitmiss_metrics[i].hit; i++) {
+    sprintf(metric_name, "STAT %s ", hitmiss_metrics[i].hit);
+    if ((metric = strstr(status, metric_name))) {
+      metric += strlen(metric_name);
+      hits += strtoull(metric, NULL, 10);
+    }
+    sprintf(metric_name, "STAT %s ", hitmiss_metrics[i].miss);
+    if ((metric = strstr(status, metric_name))) {
+      metric += strlen(metric_name);
+      misses += strtoull(metric, NULL, 10);
+    }
+  }
+  monikor_metric_push(mon, monikor_metric_integer(
+    "memcached.cache.hits", clock, hits, MONIKOR_METRIC_DELTA));
+  monikor_metric_push(mon, monikor_metric_integer(
+    "memcached.cache.misses", clock, misses, MONIKOR_METRIC_DELTA));
+}
+
+static const struct {
   char *field;
   char *name;
   uint8_t flags;
@@ -20,6 +57,7 @@ static const struct {
   {"curr_connections", "connections.current", 0},
   {"threads", "threads", 0},
   {"bytes", "size", 0},
+  {"evictions", "cache.evictions", MONIKOR_METRIC_DELTA},
   {NULL, NULL, 0}
 };
 
@@ -39,4 +77,5 @@ void memcached_poll_metrics(char *status, void *data) {
         (uint64_t)strtoull(metric, NULL, 10), metrics[i].flags));
     }
   }
+  fetch_hitmiss_metrics(mod->mon, &clock, status);
 }
