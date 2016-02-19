2933f4c5abc0a7abed8834f981f79a9f647ece33
Matthieu Rosinski <korrigan@slash16.org>
2016-02-19 11:24:56 +0100

    handling errors & server.max_send_size in monikor sender

--- src/core/send.c
+++ src/core/send.c
@@ -7,8 +7,10 @@
 #define RESPONSE_BODY_SIZE 1024
 
 typedef struct {
+  monikor_t *mon;
+  monikor_metric_list_t *metrics;
   struct curl_slist *headers;
-  char response[RESPONSE_BODY_SIZE];
+  char response[RESPONSE_BODY_SIZE + 1];
   char error[CURL_ERROR_SIZE];
   void *data;
   size_t data_size;
@@ -18,7 +20,7 @@ size_t data_handler(char *ptr, size_t size, size_t nmemb, void *userdata) {
   char *data = userdata;
 
   strncpy(data, ptr, RESPONSE_BODY_SIZE > size * nmemb ? size * nmemb : RESPONSE_BODY_SIZE);
-  data[RESPONSE_BODY_SIZE] = 0;
+  data[RESPONSE_BODY_SIZE > size * nmemb ? size * nmemb : RESPONSE_BODY_SIZE] = 0;
   return size * nmemb;
 }
 
@@ -38,17 +40,17 @@ static struct curl_slist *set_http_headers(monikor_t *mon) {
   return headers;
 }
 
-static void setup_curl_request(monikor_curl_handler_t *handler, const char *url, int timeout) {
+static void setup_curl_request(monikor_curl_handler_t *handler) {
   send_data_t *data = (send_data_t *)handler->data;
 
   strcpy(data->response, "Unknown error");
   data->error[0] = 0;
-  curl_easy_setopt(handler->curl, CURLOPT_URL, url);
+  curl_easy_setopt(handler->curl, CURLOPT_URL, data->mon->config->server.url);
   curl_easy_setopt(handler->curl, CURLOPT_NOPROGRESS, 1L);
   curl_easy_setopt(handler->curl, CURLOPT_POSTFIELDSIZE, data->data_size);
   curl_easy_setopt(handler->curl, CURLOPT_POSTFIELDS, (char *)data->data);
   curl_easy_setopt(handler->curl, CURLOPT_HTTPHEADER, data->headers);
-  curl_easy_setopt(handler->curl, CURLOPT_TIMEOUT, timeout);
+  curl_easy_setopt(handler->curl, CURLOPT_TIMEOUT, data->mon->config->server.timeout);
   curl_easy_setopt(handler->curl, CURLOPT_WRITEDATA, (void *)data->response);
   curl_easy_setopt(handler->curl, CURLOPT_WRITEFUNCTION, &data_handler);
   curl_easy_setopt(handler->curl, CURLOPT_ERRORBUFFER, data->error);
@@ -73,23 +75,25 @@ static void handle_server_response(monikor_curl_handler_t *handler, CURLcode res
   } else {
     monikor_log(LOG_INFO, "Metrics successfully sent\n");
   }
+  if (result != CURLE_OK || http_code != 200) {
+    monikor_log(LOG_INFO, "Caching %zu metrics (%zuB)\n",
+      data->metrics->size, data->metrics->data_size);
+    monikor_metric_list_concat(data->mon->metrics->cache, data->metrics);
+  }
+  monikor_metric_list_free(data->metrics);
   curl_slist_free_all(data->headers);
   free(data->data);
   free(data);
 }
 
-int monikor_send_metrics(monikor_t *mon) {
+int monikor_send_metrics(monikor_t *mon, monikor_metric_list_t *metrics) {
   monikor_curl_handler_t *handler = NULL;
   send_data_t *data = NULL;
   struct curl_slist *headers = NULL;
 
-  if (!mon->metrics->current->size) {
-    monikor_log(LOG_INFO, "No metrics to send\n");
-    return 0;
-  }
-  if (!mon->config->server_url
+  if (!mon->config->server.url
   || !(data = malloc(sizeof(*data)))
-  || monikor_metric_list_serialize(mon->metrics->current, &data->data, &data->data_size)
+  || monikor_metric_list_serialize(metrics, &data->data, &data->data_size)
   || !(headers = set_http_headers(mon))
   || !(handler = monikor_curl_handler_new(&handle_server_response, data))) {
     monikor_log(LOG_ERR, "Cannot perform metric send\n");
@@ -102,20 +106,35 @@ int monikor_send_metrics(monikor_t *mon) {
     curl_slist_free_all(headers);
     return -1;
   }
+  data->mon = mon;
+  data->metrics = metrics;
   data->headers = headers;
-  setup_curl_request(handler, mon->config->server_url, mon->config->server_timeout);
+  setup_curl_request(handler);
   monikor_io_handler_list_push_curl(&mon->io_handlers, handler);
   monikor_log(LOG_INFO, "Sending %zu metrics to %s\n",
-    mon->metrics->current->size, mon->config->server_url);
+    metrics->size, mon->config->server.url);
   return 0;
 }
 
 void monikor_send_all_metrics(monikor_t *mon) {
-  monikor_metric_list_concat(mon->metrics->current, mon->metrics->cache);
-  if (!monikor_send_metrics(mon)) {
-    monikor_metric_store_flush(mon->metrics);
-  } else {
-    monikor_log(LOG_DEBUG, "Caching %zu metrics\n", mon->metrics->current->size);
-    monikor_metric_store_cache(mon->metrics);
+  monikor_metric_list_t *metrics;
+
+  if (!mon->metrics->current->size && !mon->metrics->cache->size) {
+    monikor_log(LOG_DEBUG, "No metrics to send\n");
+    return;
   }
+  if (!(metrics = monikor_metric_list_new())) {
+    monikor_log(LOG_ERR, "Cannot send metrics: out of memory\n");
+    monikor_metric_store_cache(mon->metrics);
+    return;
+  }
+  monikor_metric_store_uncache(mon->metrics);
+  while (mon->metrics->current->size && metrics->data_size < mon->config->server.max_send_size) {
+    monikor_metric_list_push(metrics, monikor_metric_list_pop(mon->metrics->current));
+  }
+  if (monikor_send_metrics(mon, metrics)) {
+    monikor_log(LOG_INFO, "Caching %zu metrics (%zub)\n", metrics->size, metrics->data_size);
+    monikor_metric_list_concat(mon->metrics->cache, metrics);
+  }
+  monikor_metric_store_cache(mon->metrics);
 }
