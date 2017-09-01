7ea0bbd8eb7680ab269c321e44e38e95fcb65dda
Matthieu Rosinski <korrigan@slash16.org>
2017-09-01 11:08:25 +0200

    added network.interfaces option to network module

--- lib/modules/network/network.c
+++ lib/modules/network/network.c
@@ -6,23 +6,36 @@
 
 
 void *network_init(monikor_t *mon, monikor_config_dict_t *config) {
+  monikor_net_mod_t *mod;
+
   (void)mon;
-  (void)config;
-  return NULL;
+  if (!(mod = malloc(sizeof(*mod))))
+    return NULL;
+  mod->ifaces = monikor_config_dict_get_list(config, "network.interfaces");
+  return mod;
 }
 
 void network_cleanup(monikor_t *mon, void *data) {
+  monikor_net_mod_t *mod = (monikor_net_mod_t *)data;
+
   (void)mon;
-  (void)data;
+  if (!mod)
+    return;
+  strl_delete(mod->ifaces);
+  free(mod);
 }
 
 int network_poll(monikor_t *mon, void *data) {
+  monikor_net_mod_t *mod = (monikor_net_mod_t *)data;
   struct timeval now;
   int i = 0;
 
-  (void)data;
+  if (!mod) {
+    monikor_log_mod(LOG_ERR, MOD_NAME, "Invalid context data.\n");
+    return -1;
+  }
   gettimeofday(&now, NULL);
-  i += poll_network_metrics(mon, &now);
+  i += poll_network_metrics(mon, &now, mod);
   i += poll_tcp_metrics(mon, &now);
   return i;
 }
--- lib/modules/network/network.h
+++ lib/modules/network/network.h
@@ -6,7 +6,11 @@
 #define MOD_NAME "network"
 #define MNK_NET_MAX_DEV_LEN 10
 
-int poll_network_metrics(monikor_t *mon, struct timeval *clock);
+typedef struct {
+  strl_t *ifaces;
+} monikor_net_mod_t;
+
+int poll_network_metrics(monikor_t *mon, struct timeval *clock, monikor_net_mod_t *mod);
 int poll_tcp_metrics(monikor_t *mon, struct timeval *clock);
 
 #endif /* end of include guard: network_H_ */
