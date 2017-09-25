da6b57c4ac68f9232bb13c3512fec85eae04b01c
Matthieu Rosinski <korrigan@slash16.org>
2017-09-25 18:50:58 +0200

    polished postgresql module, added config, fix a few bugs

--- lib/modules/postgresql/pollgresql.c
+++ lib/modules/postgresql/pollgresql.c
@@ -8,118 +8,172 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
-#include <libpq-fe.h>
+#include <postgresql/libpq-fe.h>
 
-int max_connection(monikor_t mon, struct timeval *clock, PGresult *result)
-{
+#include "postgresql.h"
+
+int max_connection(monikor_t *mon, postgresql_module_t *mod,
+struct timeval *clock, PGresult *result) {
+  char metric_name[1024];
+  char *value;
+
+  sprintf(metric_name, "postgresql.connections.max.%s", mod->database);
+  if (!(value = PQgetvalue(result, 0, 1)))
+    return 0;
   return monikor_metric_push(mon, monikor_metric_integer(
-    "postgresql.max_connections", clock, strtoull(PQgetvalue(result, 0, 1), NULL, 0), 0));
+    metric_name, clock, strtoull(value, NULL, 0), 0));
 }
 
-int database_size(monikor_t mon, struct timeval *clock, PGresult *result)
-{
+int database_size(monikor_t *mon, postgresql_module_t *mod,
+struct timeval *clock, PGresult *result) {
+  char metric_name[1024];
+  char *value;
+
   // Query return databse_size as a string stocked at 0/0 coordinates in result
   // Result seems to be in bytes
+  if (!(value = PQgetvalue(result, 0, 0)))
+    return 0;
+  sprintf(metric_name, "postgresql.database_size.%s", mod->database);
   return monikor_metric_push(mon, monikor_metric_integer(
-    "postgresql.database_size", clock, strtoull(PQgetvalue(result, 0, 0), NULL, 0), 0));
+    metric_name, clock, strtoull(value, NULL, 0), 0));
 }
 
-int pg_locks(monikor_t mon, struct timeval *clock, PGresult *result)
-{
+int pg_locks(monikor_t *mon, postgresql_module_t *mod, struct timeval *clock, PGresult *result) {
+  char metric_name[1024];
+  char *value;
+
+  sprintf(metric_name, "postgresql.locks.%s", mod->database);
   // This table stock lock information, probably needs to be parsed by locktype
+  if (!(value = PQgetvalue(result, 0, 0)))
+    return 0;
   return monikor_metric_push(mon, monikor_metric_integer(
-    "locks", clock, PQntuples(result), 0));
+    metric_name, clock, strtoull(value, NULL, 10), 0));
 }
 
-int stat_activity(monikor_t mon, struct timeval *clock, PGresult *result)
-{
+int stat_activity(monikor_t *mon, postgresql_module_t *mod,
+struct timeval *clock, PGresult *result) {
+  char metric_name[1024];
+
+  sprintf(metric_name, "postgresql.connections.active.%s", mod->database);
   // This table stock user-related informations, I believe each row represent an User, need confirmation
   return monikor_metric_push(mon, monikor_metric_integer(
-    "active.connections", clock, PQntuples(result), 0));
+    metric_name, clock, PQntuples(result), 0));
 }
 
-int stat_database(monikor_t mon, struct timeval *clock, PGresult *result)
-{
+int stat_database(monikor_t *mon, postgresql_module_t *mod,
+struct timeval *clock, PGresult *result) {
   int n = 0;
+  char metric_name[1024];
+  char *value;
 
   // Postgresql transactions
-  n += monikor_metric_push(mon, monikor_metric_integer("commits/s",
-    clock, strtoull(PQgetvalue(result, 0, 3), NULL, 0), MONIKOR_METRIC_TIMEDELTA));
+  sprintf(metric_name, "postgresql.transactions.commits.%s", mod->database);
+  if ((value = PQgetvalue(result, 0, 3)))
+    n += monikor_metric_push(mon, monikor_metric_integer(metric_name,
+      clock, strtoull(value, NULL, 0), MONIKOR_METRIC_TIMEDELTA));
 
-  n += monikor_metric_push(mon, monikor_metric_integer("rollback/s",
-    clock, strtoull(PQgetvalue(result, 0, 4), NULL, 0), MONIKOR_METRIC_TIMEDELTA));
+  sprintf(metric_name, "postgresql.transactions.rollbacks.%s", mod->database);
+  if ((value = PQgetvalue(result, 0, 4)))
+    n += monikor_metric_push(mon, monikor_metric_integer(metric_name,
+      clock, strtoull(value, NULL, 0), MONIKOR_METRIC_TIMEDELTA));
 
   // Postgresql queries
-  n += monikor_metric_push(mon, monikor_metric_integer("postgresql.rows_returned",
-    clock, strtoull(PQgetvalue(result, 0, 7), NULL, 0), MONIKOR_METRIC_TIMEDELTA));
+  sprintf(metric_name, "postgresql.rows.returned.%s", mod->database);
+  if ((value = PQgetvalue(result, 0, 7)))
+    n += monikor_metric_push(mon, monikor_metric_integer(metric_name,
+      clock, strtoull(value, NULL, 0), MONIKOR_METRIC_TIMEDELTA));
 
-  n += monikor_metric_push(mon, monikor_metric_integer("postgresql.rows_fetched",
-    clock, strtoull(PQgetvalue(result, 0, 8), NULL, 0), MONIKOR_METRIC_TIMEDELTA));
+  sprintf(metric_name, "postgresql.rows.fetched.%s", mod->database);
+  if ((value = PQgetvalue(result, 0, 8)))
+    n += monikor_metric_push(mon, monikor_metric_integer(metric_name,
+      clock, strtoull(value, NULL, 0), MONIKOR_METRIC_TIMEDELTA));
 
-  n += monikor_metric_push(mon, monikor_metric_integer("postgresql.rows_inserted",
-    clock, strtoull(PQgetvalue(result, 0, 9), NULL, 0), MONIKOR_METRIC_TIMEDELTA));
+  sprintf(metric_name, "postgresql.rows.inserted.%s", mod->database);
+  if ((value = PQgetvalue(result, 0, 9)))
+    n += monikor_metric_push(mon, monikor_metric_integer(metric_name,
+      clock, strtoull(value, NULL, 0), MONIKOR_METRIC_TIMEDELTA));
 
-  n += monikor_metric_push(mon, monikor_metric_integer("postgresql.rows_updated",
-    clock, strtoull(PQgetvalue(result, 0, 10), NULL, 0), MONIKOR_METRIC_TIMEDELTA));
+  sprintf(metric_name, "postgresql.rows.updated.%s", mod->database);
+  if ((value = PQgetvalue(result, 0, 10)))
+    n += monikor_metric_push(mon, monikor_metric_integer(metric_name,
+      clock, strtoull(value, NULL, 0), MONIKOR_METRIC_TIMEDELTA));
 
-  n += monikor_metric_push(mon, monikor_metric_integer("postgresql.rows_deleted",
-    clock, strtoull(PQgetvalue(result, 0, 11), NULL, 0), MONIKOR_METRIC_TIMEDELTA));
+  sprintf(metric_name, "postgresql.rows.deleted.%s", mod->database);
+  if ((value = PQgetvalue(result, 0, 11)))
+    n += monikor_metric_push(mon, monikor_metric_integer(metric_name,
+      clock, strtoull(value, NULL, 0), MONIKOR_METRIC_TIMEDELTA));
 
-    // Postgresql locks
-  n += monikor_metric_push(mon, monikor_metric_integer("deadlocks/s",
-    clock, strtoull(PQgetvalue(result, 0, 15), NULL, 0), MONIKOR_METRIC_TIMEDELTA));
+  // Postgresql deadlocks
+  sprintf(metric_name, "postgresql.deadlocks_per_second.%s", mod->database);
+  if ((value = PQgetvalue(result, 0, 15)))
+    n += monikor_metric_push(mon, monikor_metric_integer(metric_name,
+      clock, strtoull(value, NULL, 0), MONIKOR_METRIC_TIMEDELTA));
   return n;
 }
 
-int postgresql_poll_metrics(monikor_t *mon, struct timeval *clock))
-{
-  uint64_t value;
+typedef int (*poll_func_t)(monikor_t *, postgresql_module_t *, struct timeval *, PGresult *);
+static const poll_func_t pollers[] = {
+  stat_activity,
+  stat_database,
+  database_size,
+  pg_locks,
+  max_connection,
+  NULL
+};
+
+static const char *sql_query_format = "SELECT * FROM pg_stat_activity WHERE datname = %s;"
+  "SELECT * FROM pg_stat_database WHERE datname = %s;"
+  "SELECT pg_database_size(%s);"
+  "SELECT COUNT(1) FROM pg_locks AS l INNER JOIN pg_class AS pc ON (l.relation = pc.oid) WHERE relname NOT LIKE 'pg\\_%%' AND l.mode IS NOT NULL;"
+  "SELECT * FROM pg_settings WHERE name = 'max_connections';";
+
+static const char *connect_fmt =
+  "host=%s port=%s dbname=postgres user=%s password=%s connect_timeout=2";
+
+int postgresql_poll_metrics(monikor_t *mon, struct timeval *clock, postgresql_module_t *mod) {
   int n = 0;
-  PGconn *connectdb;
+  PGconn *dbh = NULL;
   PGresult *result;
-  ExecStatusType status;
+  char *sql_query = NULL;
+  char *escaped_db_name = NULL;
+  char *connect_opts = NULL;
 
-  // Connecting to database and checking for errors
-  connectdb = PQsetdb("127.0.0.1", NULL, NULL, NULL, "postgres");
-  if (PQstatus(connectdb) != CONNECTION_OK)
-    return 0;
+  if (asprintf(&connect_opts, connect_fmt, mod->host, mod->port, mod->user, mod->password) <= 0)
+    goto end;
+  dbh = PQconnectdb(connect_opts);
+  if (PQstatus(dbh) != CONNECTION_OK) {
+    monikor_log_mod(LOG_ERR, MOD_NAME,
+      "cannot connect to postgresql: %s", PQerrorMessage(dbh));
+    goto end;
+  }
+  if (!(escaped_db_name = PQescapeLiteral(dbh, mod->database, strlen(mod->database)))
+  || asprintf(&sql_query, sql_query_format, escaped_db_name, escaped_db_name, escaped_db_name) <= 0) {
+    monikor_log_mod(LOG_ERR, MOD_NAME, "cannot prepare SQL query\n");
+    goto end;
+  }
+  if (!PQsendQuery(dbh, sql_query)) {
+    monikor_log_mod(LOG_ERR, MOD_NAME,
+      "cannot execute postgresql query: %s", PQerrorMessage(dbh));
+    goto end;
+  }
 
-  //First query : pg_stat_activity
-  result = PQexec(connectdb, "select * from pg_stat_activity");
-  status = PQresultStatus(result); // Check for memory leaks
-  if (strcmp(PQresStatus(status), "PGRES_TUPLES_OK") == 0)
-    n += stat_activity(mon, clock, result);
-  PQclear(result);
+  for (size_t i = 0; pollers[i]; i++) {
+    result = PQgetResult(dbh);
+    if (!result)
+      continue;
+    if (PQresultStatus(result) == PGRES_TUPLES_OK) {
+      n += pollers[i](mon, mod, clock, result);
+    } else {
+      monikor_log_mod(LOG_ERR, MOD_NAME, "bad result: %s\n",
+        PQresStatus(PQresultStatus(result)));
+    }
+    PQclear(result);
+  }
 
-  //Second query : pg_stat_database
-  result = PQexec(connectdb, "select * from pg_stat_database");
-  status = PQresultStatus(result); // Check for memory leaks
-  if (strcmp(PQresStatus(status), "PGRES_TUPLES_OK") == 0)
-    n += stat_database(mon, clock, result);
-  PQclear(result);
-
-  //Third query : pg_locks
-  result = PQexec(connectdb, "select * from pg_locks");
-  status = PQresultStatus(result); // Check for memory leaks
-  if (strcmp(PQresStatus(status), "PGRES_TUPLES_OK") == 0)
-    n += pg_locks(mon, clock, result);
-  PQclear(result);
-
-  //Fourth query : pg_database_size
-  result = PQexec(connectdb, "select pg_database_size('postgres');");
-  status = PQresultStatus(result);
-  if (strcmp(PQresStatus(status), "PGRES_TUPLES_OK") == 0)
-    n += database_size(mon, clock, result);
-  PQclear(result);
-
-  //Fifth query : max connection
-  result = PQexec(connectdb, "select * from pg_settings where name = 'max_connections';");
-  status = PQresultStatus(result);
-  if (strcmp(PQresStatus(status), "PGRES_TUPLES_OK") == 0)
-    n += max_connection(mon, clock, result);
-  PQclear(result);
-
-  //free memory
-  PQfinish(connectdb);
+end:
+  PQfreemem(escaped_db_name);
+  free(sql_query);
+  free(connect_opts);
+  PQfinish(dbh);
   return n;
 }
--- lib/modules/postgresql/postgresql.h
+++ lib/modules/postgresql/postgresql.h
@@ -4,5 +4,18 @@
 #include "monikor.h"
 
 #define MOD_NAME "postgresql"
+#define POSTGRESQL_DEFAULT_HOST "127.0.0.1"
+#define POSTGRESQL_DEFAULT_PORT "5432"
+#define MONIKOR_POSTGRES_MAX_INSTANCE_LENGTH 64
+
+typedef struct {
+  char *host;
+  char *port;
+  char *user;
+  char *password;
+  char *database;
+} postgresql_module_t;
+
+int postgresql_poll_metrics(monikor_t *mon, struct timeval *clock, postgresql_module_t *mod);
 
 #endif /* end of include guard: postgresql_H_ */
