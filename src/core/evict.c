66c938aa381cdd611907be38ed2a9f31d67d25a5
Matthieu Rosinski <korrigan@slash16.org>
2023-08-09 18:26:12 +0200

    rename local variable in evict.c

--- src/core/evict.c
+++ src/core/evict.c
@@ -3,12 +3,12 @@
 #include "monikor.h"
 
 void monikor_evict_metrics(monikor_t *mon) {
-  struct timeval clock;
+  struct timeval max_delta_clock;
   size_t n;
 
-  gettimeofday(&clock, NULL);
-  clock.tv_sec -= 3 * mon->config->poll_interval;
-  n = monikor_metric_store_evict_delta(mon->metrics, &clock)
+  gettimeofday(&max_delta_clock, NULL);
+  max_delta_clock.tv_sec -= 3 * mon->config->poll_interval;
+  n = monikor_metric_store_evict_delta(mon->metrics, &max_delta_clock)
     + monikor_metric_store_evict_cache(mon->metrics, mon->config->cache.max_size);
   monikor_log(LOG_INFO, "Evicted %zu metrics from caches\n", n);
 }
