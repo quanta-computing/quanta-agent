504a171a89feebc960c4322cd90f3a3824613fe3
Paul Oureib <poureib@centreon.com>
2026-06-17 09:50:45 +0200

    include missing dependencies

--- packaging/scripts/quanta-agent-base.postinstall.sh
+++ packaging/scripts/quanta-agent-base.postinstall.sh
@@ -9,6 +9,10 @@ getent group quanta-agent > /dev/null || groupadd -r quanta-agent
 getent passwd quanta-agent > /dev/null || \
   useradd -r -g quanta-agent -d /var/run/quanta -s /sbin/nologin \
   -c "Quanta agent" quanta-agent
+if [ ! -d /var/run/quanta ]; then
+  mkdir /var/run/quanta
+  chown quanta-agent:quanta-agent /var/run/quanta
+fi
 
 systemctl daemon-reload ||:
 systemctl unmask quanta-agent.service ||:
